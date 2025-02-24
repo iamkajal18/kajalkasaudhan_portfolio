128957ff99f9a6116f3eed29f4670466ad6ac3ba
iamkajal18 <152779714+iamkajal18@users.noreply.github.com>
2025-02-24 15:21:42 +0530

    refactor: replace Navbar component with TypeScript version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,10 @@ import { useState,useEffect } from 'react';
 
 
 function Navbar() {
-  const [user, setUser]= useState({});
-
+ 
   const {data:session, status}= useSession();
   
-  useEffect(
-    ()=>{
-      if( status==="authenticated"){
-        setUser(session?.user|| {})
-      }
-    },[session]
-  )
- 
+
   return (
     <div className="sticky top-0 z-50">
 
@@ -31,13 +23,13 @@ function Navbar() {
   <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
         <span className="sr-only">Open user menu</span>
-        <img className="w-8 h-8 rounded-full" src={user.image} alt="user photo"/>
+        <img className="w-8 h-8 rounded-full" src={session?.user?.image || ""} alt="user photo"/>
       </button>
 
       <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
         <div className="px-4 py-3">
-          <span className="block text-sm text-gray-900 dark:text-white">{user.name}</span>
-          <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
+          <span className="block text-sm text-gray-900 dark:text-white">{session?.user?.name}</span>
+          <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{session?.user?.email}</span>
         </div>
         <ul className="py-2" aria-labelledby="user-menu-button">
           <li>
